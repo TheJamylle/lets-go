2c5d02fcafba7e72e9f761f72960c677df1e3d85
Jamylle <jamyllemagal@gmail.com>
2024-09-02 17:28:39 -0300

    Initial

---
+++ hello.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func showIntro() {
+	name := "My"
+	version := 1.0
+	fmt.Println("Olá", name)
+	fmt.Println("This program is currently on version ", version)
+}
+
+func showMenu() {
+	fmt.Println("1 - Monitorar")
+	fmt.Println("2 - Logs")
+	fmt.Println("0 - Sair")
+}
+
+func getCommand() int {
+	var something int
+	fmt.Scan(&something)
+
+	return something
+}
+
+func startMonitoring() {
+	fmt.Println("Monitorando...")
+	sites := []string{"https://www.izirh.io", "https://www.alura.com.br", "https://www.google.com"}
+
+	loop := 0
+	for len(sites) > loop {
+		response, _ := http.Get(sites[loop])
+
+		if response.StatusCode == 200 {
+			fmt.Println("Site ", sites[loop], "está funcionando corretamente")
+		} else {
+			fmt.Println("Deu problema. Status:", response.StatusCode)
+		}
+
+		loop++
+	}
+
+}
+
+func main() {
+
+	showIntro()
+
+	showMenu()
+
+	for {
+		something := getCommand()
+
+		switch something {
+		case 1:
+			startMonitoring()
+
+		case 2:
+			fmt.Println("Exibindo logs...")
+
+		case 0:
+			fmt.Println("Bye")
+			os.Exit(0)
+
+		default:
+			fmt.Println("Comando inválido")
+			os.Exit(-1)
+		}
+	}
+}
